ca4b7cf2b914c9fbfd1d8c0b5b9f184e0be29e29
LizethCalderon <48306650+LizethCalderon@users.noreply.github.com>
2019-03-19 19:40:37 -0600

    Add files via upload

---
+++ NumMulti/numeros.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumMulti
+{
+    class numeros
+    {
+        int num1;
+
+        public int Num1
+        {
+            get { return num1; }
+            set { num1 = value; }
+        }
+        int num2;
+
+        public int Num2
+        {
+            get { return num2; }
+            set { num2 = value; }
+        }
+
+        private void Multi(){
+            Console.WriteLine(" Ingrese el primer entero");
+                  if num1 = 1;
+
+
+
+
+
+        
+        }
+    }
+}
